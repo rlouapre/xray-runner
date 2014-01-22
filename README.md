386a3900a3779c0930f4bc8e6dceba087a9aff67
Richard Louapre <richard.louapre@gmail.com>
2014-01-22 07:43:13 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,98 @@
-xray-runner
-===========
+# grunt-xray-runner
 
-Grunt task to run XQuery unit test with XRay
+> This plugin uses XRay to automate testing of XQuery in Marklogic
+
+## Getting Started
+This plugin requires Grunt `~0.4.2`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-xray-runner --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-xray-runner');
+```
+
+## The "xray_runner" task
+
+### Overview
+In your project's Gruntfile, add a section named `xray_runner` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  xray_runner: {
+    all: {
+      settings: {
+        url: 'http://localhost:9999',
+        testDir: 'test',
+        files: ['test/**/*.xqy']
+      }
+    }
+  }
+});
+```
+
+### Settings
+
+#### settings.url
+Type: `String`
+
+Base url of ML application server (XRay must be installed and available in ```{settings.url}/xray``` directory).
+
+#### settings.testDir
+Type: `String`
+Default value: `test`
+
+Directory name where unit test are located.
+
+#### settings.files
+Type: `String Array`
+Default value: `test`
+
+Unit test files to execute (support matching globbing pattern).
+
+### Usage Examples
+
+#### Default Options
+In this example, the default options are used to do something with whatever. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result would be `Testing, 1 2 3.`
+
+```js
+grunt.initConfig({
+  xray_runner: {
+    all: {
+      settings: {
+        url: 'http://localhost:9999',
+        testDir: 'test',
+        files: ['test/**/*.xqy']
+      }
+    }
+  }
+});
+```
+
+#### Custom Options
+In this example, custom options are used to do something else with whatever else. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result in this case would be `Testing: 1 2 3 !!!`
+
+```js
+grunt.initConfig({
+  xray_runner: {
+    options: {
+      separator: ': ',
+      punctuation: ' !!!',
+    },
+    files: {
+      'dest/default_options': ['src/testing', 'src/123'],
+    },
+  },
+});
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
---
+++ tasks/xray-runner-task.js
@@ -0,0 +1,121 @@
+/*
+ * grunt-xray-runner
+ * https://github.com/rlouapre/xray-runner
+ *
+ * Copyright (c) 2014 Richard Louapre
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('xray_runner', 'Execute Xquery unit tests in Xray', function() {
+
+    // TODO: Add support for argument in command line: grunt xray --files=fail.xqy
+    // console.log('files: ' + grunt.option('files'));
+    var template = (this.data.template !== undefined) ? this.data.template: 'xray-runner/tasks/lib/xray-runner.js';
+    var verbose = (grunt.option('verbose') !== undefined) ? grunt.option('verbose') : false;
+    var path = require('path');
+    var _ = require('lodash');
+    var jasmine = require('jasmine-node');
+    var util = require('util');
+
+    if (! grunt.file.exists(path.resolve(template))) {
+      throw new Error('Cannot find template ' + template);
+    }
+
+    if (verbose) {
+      grunt.verbose.writeln('name [' + this.name + '] - target [' + this.target + ']');
+      grunt.verbose.writeln('data ' + JSON.stringify(this.data.settings, null, 1));
+    }
+
+    var files = [];
+
+    _.each(grunt.file.expand(this.data.settings.files), function(file) {
+      files.push(file);
+    });
+
+    if (files.length > 0) {
+      this.data.settings.files = files;
+    }
+
+    grunt.verbose.writeln('files ' + this.data.settings.files);
+    grunt.verbose.writeln('testDir ' + this.data.settings.testDir);
+    var testDir = this.data.settings.testDir;
+    var isModule = (_.find(this.data.settings.files, function(file) {
+      return file.indexOf(testDir) === 0;
+    }) === undefined);
+
+    if (isModule) {
+      this.data.settings.files = undefined;
+    } else {
+      files = [];
+      _.each(this.data.settings.files, function (file) {
+        files.push(path.basename(file));
+      });
+      this.data.settings.files = files;
+    }
+
+    if (Array.isArray(this.data.settings.files)) {
+      this.data.settings.files = this.data.settings.files.join(',');
+    }
+
+    // Provide all settings to xray-runner
+    _.each(this.data.settings, function(value, key) {
+      if (value !== undefined) {
+        process.env['xray.settings.' + key] = value;
+      } else {
+        delete process.env['xray.settings.' + key];
+      }
+    });
+
+    var done = this.async();
+    var forceExit = false;
+
+    var onComplete = function(runner, log) {
+      var exitCode;
+      util.print('\n');
+      if (runner.results().failedCount === 0) {
+        exitCode = 0;
+      } else {
+        exitCode = 1;
+
+        if (forceExit) {
+          process.exit(exitCode);
+        }
+      }
+      jasmine.getGlobal().jasmine.currentEnv_ = undefined;
+      done();
+    };
+
+    var options = {
+      specFolders:     [template],
+      isVerbose:       verbose,
+      showColors:      true,
+      teamcity:        false,
+      useRequireJs:    true,
+      coffee:          false,
+      onComplete:      onComplete,
+      jUnit: {
+        report: false,
+        savePath : './build/reports/jasmine/',
+        useDotNotation: true,
+        consolidate: true
+      }
+    };
+
+    try {
+      // since jasmine-node@1.0.28 an options object need to be passed
+      jasmine.executeSpecsInFolder(options);
+      grunt.verbose.ok();
+    } catch (e) {
+      grunt.error('Failed to execute "jasmine.executeSpecsInFolder": ' + e.stack);
+    }
+
+  });
+
+};
